7dad6227f6e4d405ed0fed069689c6bd47c4e8e8
KordianKlimas <k.klimas@student.maastrichtuniversity.nl>
2024-04-20 19:45:13 +0200

    Providing UML class diagram with brief description.

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -3,8 +3,15 @@ package com.cardio_generator.generators;
 import com.cardio_generator.outputs.OutputStrategy;
 
 /**
- * Interface for generating artificial patient data for chosen patient
+ * Interface for generating artificial patient data for chosen patient, with defined output strategy.
+ *
  */
 public interface PatientDataGenerator {
+    /**
+     * method generating artificial patient data for chosen patient, using chosen output strategy.
+     *
+     * @param patientId The ID of the patient.
+     * @param outputStrategy OutputStrategy  (interface)
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
